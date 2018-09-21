c4ed123b26228998ca1f3378e1818ed28089deda
pws019 <510114321@email.com>
2018-09-22 00:57:58 +0800

    渐变色

---
+++ 2-5/渐变色/index.js
@@ -0,0 +1,34 @@
+
+function drawCanvas(id, createLinearFn) {
+  var canvas  = document.getElementById(id),
+  context =  canvas.getContext('2d'),
+  gradient = createLinearFn(context, canvas)
+
+  gradient.addColorStop(0, 'blue');
+  gradient.addColorStop(0.25, 'white');
+  gradient.addColorStop(0.5, 'purple');
+  gradient.addColorStop(0.75, 'red');
+  gradient.addColorStop(1, 'yellow');
+
+  context.strokeStyle = gradient;
+  context.strokeRect(0, 0, canvas.width, canvas.height);
+
+  context.fillStyle = gradient;
+  context.fillRect(0,0, canvas.width, canvas.height)
+}
+
+drawCanvas('canvas1', (context, canvas)=>{
+  return context.createLinearGradient(0, 0, canvas.width, 0);
+})
+
+drawCanvas('canvas2', (context, canvas)=>{
+  return context.createLinearGradient(0, 0, 0, canvas.height);
+})
+
+drawCanvas('canvas3', (context, canvas)=>{
+  return context.createLinearGradient(0, 0, canvas.width, canvas.height);
+})
+
+drawCanvas('canvas4', (context, canvas)=>{
+  return context.createLinearGradient(0, 0, 0, canvas.height/2);
+})
